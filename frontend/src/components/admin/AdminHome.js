8f28cefbe037c39fc32b320e253f5cf13bd7b229
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2025-10-06 12:23:05 +0200

    Refactor AdminHome component; simplify navigation links with reusable getLink function and remove unused book fetching logic

--- frontend/src/components/admin/AdminHome.js
+++ frontend/src/components/admin/AdminHome.js
@@ -1,61 +1,33 @@
 import {NavLink} from "react-router-dom";
 import {usePageTitle} from "../utils/usePageTitle";
-import {useQuery} from "@tanstack/react-query";
-import axios from "axios";
-
-const fetchBooks = async () => {
-    const {data} = await axios.get('/api/admin/libri/light-all');
-    return data;
-}
 
 const AdminHome = () => {
-    const {data: books, isLoading, error} = useQuery({
-        queryKey: ['adminBooks'],
-        queryFn: fetchBooks,
-        select: (data) => data.sort((a, b) => a.id - b.id), // Ordina i libri per ID in ordine crescente
-    });
 
     usePageTitle('Admin Dashboard');
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error loading books.</div>;
+    const getLink = (link, color, emoji, text) => {
+        return (
+            <NavLink to={link}
+                     className={`inline-block mb-4 ml-4 px-4 py-2 bg-${color}-500 text-white rounded hover:bg-${color}-600`}>
+                <div className="flex items-center">
+                    <i className={`bxr ${emoji} mr-2`}></i> {text}
+                </div>
+            </NavLink>
+        );
     }
 
     return (
         <div className="container mx-auto p-4">
             <h3 className="text-2xl font-bold mb-4">Dashboard</h3>
 
-            <NavLink to="/admin/book"
-                     className="inline-block mb-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-                <i className="bxr bxs-book mr-2"></i> Book List
-            </NavLink>
-
-
-            <NavLink to={"/admin/order"}
-                     className="inline-block mb-4 ml-4 px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600">
-                <i className="bxr bxs-cart mr-2"></i> Order List
-            </NavLink>
-
-
-
-            <NavLink to={"/admin/reso"}
-                     className="inline-block mb-4 ml-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
-                <i className="bxr bxs-refresh-cw-alt mr-2"></i> Return List
-            </NavLink>
-
-
-            <NavLink to={"/admin/user"}
-                     className="inline-block mb-4 ml-4 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
-                <i className="bxr bxs-user mr-2"></i> User List
-            </NavLink>
+            {getLink("/admin/book", "blue", "bxs-book", "Book List")}
+            {getLink("/admin/order", "purple", "bxs-cart", "Order List")}
+            {getLink("/admin/reso", "red", "bxs-refresh-cw-alt", "Return List")}
+            {getLink("/admin/user", "green", "bxs-user", "User List")}
         </div>
 
 
-);
+    );
 }
 
 export default AdminHome;
