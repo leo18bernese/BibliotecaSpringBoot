f4e5e1039a020b7e889fde4bead8b8c52acb27c0
itz_leoo <76162316+leo18bernese@users.noreply.github.com>
2025-11-03 16:18:21 +0100

    Enhance LiteCategory2 component; add admin links for category and image management in admin mode

--- frontend/src/components/category/LiteCategory2.js
+++ frontend/src/components/category/LiteCategory2.js
@@ -1,6 +1,8 @@
 import {useQuery} from "@tanstack/react-query";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import React, {useContext} from "react";
+import {UserContext} from "../user/UserContext";
 
 const fetchCategoryById = async (categoryId) => {
     const {data} = await axios.get(`/api/categories/${categoryId}`);
@@ -14,7 +16,7 @@ const fetchImageById = async (categoryId) => {
 
 
 const LiteCategory2 = ({categoryID, category: providedCategory}) => {
-
+    const {user, isAdmin, isAdminMode} = useContext(UserContext);
     const navigate = useNavigate();
     const categoryId = categoryID || (providedCategory?.id);
 
@@ -44,10 +46,37 @@ const LiteCategory2 = ({categoryID, category: providedCategory}) => {
                  onClick={() => navigate(`/category/${categoryId}`)}
             >
 
-                <img src={`/api/categories/${categoryId}/image`}
-                    className="w-full h-48 object-cover mb-4 "
-                    alt={category.name}
-                />
+                <div className="relative">
+                    <img src={`/api/categories/${categoryId}/image`}
+                         className="w-full h-48 object-cover mb-4 "
+                         alt={category.name}
+                    />
+
+                    {user && isAdminMode() && (
+                        <>
+                        <Link to={`/admin/category/${categoryID}`} target={`_blank`}>
+                            <div
+                                className="absolute bottom-2 left-2 bg-yellow-500 text-white px-2 py-1 rounded-md font-semibold
+                                        hover:bg-yellow-600 transition-colors"
+                                onClick={(e) => e.stopPropagation()}
+                            >
+                                <i className='bx bx-edit-alt text-xl'></i>
+                            </div>
+                        </Link>
+
+                            <Link to={`/admin/category/${categoryID}/images`} target={`_blank`}>
+                                <div
+                                    className="absolute bottom-2 left-12 bg-yellow-500 text-white px-2 py-1 rounded-md font-semibold
+                                        hover:bg-yellow-600 transition-colors"
+                                    onClick={(e) => e.stopPropagation()}
+                                >
+                                    <i className='bx bx-image text-xl'></i>
+                                </div>
+                            </Link>
+
+                        </>
+                    )}
+                </div>
 
                 <div className="text-center text-gray-700 ">
                     <h3 className="font-bold  my-5">{category.name}</h3>
